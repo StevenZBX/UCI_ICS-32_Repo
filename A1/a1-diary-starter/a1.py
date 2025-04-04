61718af5eaa4433913329f83918785330883d766
ZBX <boxuanz3@uci.edu>
2025-04-04 11:43:40 -0700

    Initialized a1.py file

--- A1/a1-diary-starter/a1.py
+++ A1/a1-diary-starter/a1.py
@@ -2,6 +2,12 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# NAME Boxuan Zhang
+# EMAIL boxuanz3@uci.edu
+# STUDENT ID 95535906
+
+import command_parser
+
+
+if __name__=="__main__":
+    pass
