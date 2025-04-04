db2c1e06d203a300407806ac928ddfea1a59fdff
ZBX <boxuanz3@uci.edu>
2025-04-04 11:29:54 -0700

    Create command functions in the command file

--- A1/a1-diary-starter/command_parser.py
+++ A1/a1-diary-starter/command_parser.py
@@ -2,6 +2,24 @@
 
 # Replace the following placeholders with your information.
 
-# NAME
-# EMAIL
-# STUDENT ID
+# NAME Boxuan Zhang
+# EMAIL boxuanz3@uci.edu
+# STUDENT ID 95535906
+
+def command_c(): #create
+    pass
+
+def command_d(): #delete
+    pass
+
+def command_o(): #load
+    pass
+
+def command_e(): #edit
+    pass
+
+def command_p(): #print
+    pass
+
+def command_p(): #quit
+    pass
